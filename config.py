98fde7d70a8bb73aa36e2c1b018b40112410426f
Piotr Chomicki <109987107+xioulin@users.noreply.github.com>
2024-01-18 16:35:53 -0500

    reddit scrape

---
+++ config.py
@@ -0,0 +1,6 @@
+
+
+
+#lower level app on reddit
+client_id="tublKPzQgRNsxiy1PDXABA"
+client_secret="12nSwgUCNjqVddL5OMwRQVGpVrkGsg"
---
+++ reddit_scrape.py
@@ -0,0 +1,31 @@
+import praw
+import config
+
+reddit=praw.Reddit(
+    config.client_id,
+    config.client_secret,
+    user_agent="testscript by u/Steelkilt",
+    redirect_uri='https://localhost:8081'
+)
+
+subreddit= reddit.subreddit('Catholicism')
+
+
+top_posts= subreddit.top(limit=5)
+new_posts= subreddit.new(limit=5)
+
+for posts in top_posts:
+    print(posts.title)
+
+
+post= reddit.submission(id='h0qb82')
+print(post.title)
+comments= post.comments
+
+for comment in comments[:2]:
+    print(comment.body)
+
+# print(reddit.auth.url(scopes=['identity'],state="...d",duration="permanent"))
+# code='TCqYNX0uK-SkQO_loAdEaok0kcq0YQ#_'
+# print(reddit.auth.authorize(code))
+# print(reddit.user.me())
