aa8c93b8f5a72f77a47d95d524d7837d877d22ed
Piotr Chomicki <109987107+xioulin@users.noreply.github.com>
2024-01-17 21:21:36 -0500

    first things scrape

--- Chatterbot/Conservo_Bot/getYolked.py
+++ Chatterbot/Conservo_Bot/getYolked.py
@@ -2,22 +2,11 @@ import requests
 from bs4 import BeautifulSoup
 
 
-url='https://www.lifesitenews.com/blogs/canadas-military-stigmatized-straight-white-dudes-now-it-has-a-recruitment-crisis/'
+
+url='https://www.firstthings.com/web-exclusives/2024/01/more-confusion-about-same-sex-blessings'
 r= requests.get(url)
 
 soup= BeautifulSoup(r.content, 'html.parser')
 
-s=soup.find(id='primary')
-lines= s.find_all('p')
-# print(lines)
-# print(lines[:25])
-lines_list= list(lines)
-# print(lines_list)
-print(lines_list)
-# content=s.find_all('p')
-# print(content)
-
-
-
-
-
+elems = soup.find('div', class_='primary')
+print(elems)
