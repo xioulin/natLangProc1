66c750be08ce47d1be99691ac7a33691646392bd
Piotr Chomicki <109987107+xioulin@users.noreply.github.com>
2024-04-05 23:12:22 -0400

    abandon javascript, work on flask

--- Dead Presidents/deadprez.js
+++
@@ -1,5 +0,0 @@
-const deadPrez= ['washington','lincoln','trump','nixon','reagan']
-
-for (let i=0; i<deadPrez.length;i++){
-    console.log(deadPrez[i])
-}
--- Dead_Presidents/scrapePrez.py
+++ Dead_Presidents/scrapePrez.py
@@ -12,21 +12,21 @@ results= soup.find(class_="search-results grid-view public-domain-results public
 prez_images= results.find_all('img')
 
 base_url= 'https://www.loc.gov/'
-# url_ext= prez_images[0].attrs['src']
+# url_ext= prez_img[0].attrs['src']
 # full_url = base_url+url_ext
 # r= requests.get(full_url, stream=True)
 
 
 """
-this for loop runs through the entire prez_images
+this for loop runs through the entire prez_img
 
 
-for i in range(len(prez_images)):
-    url_ext = prez_images[i].attrs['src']
+for i in range(len(prez_img)):
+    url_ext = prez_img[i].attrs['src']
     full_url = base_url + url_ext
     r = requests.get(full_url, stream=True)
     if r.status_code==200:
-        with open(f'images/{getPrez(prez_images[i].attrs["src"])}.jpg','wb') as f:
+        with open(f'prez_img/{getPrez(prez_img[i].attrs["src"])}.jpg','wb') as f:
             r.raw.decode_content=True
             shutil.copyfileobj(r.raw,f)
 """
