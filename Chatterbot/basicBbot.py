efe51e37d615d065dfd1e8eca9eaca0cb4086fd3
Piotr Chomicki <109987107+xioulin@users.noreply.github.com>
2024-04-16 14:57:13 -0400

    ...

--- Chatterbot/basicBbot.py
+++ Chatterbot/basicBbot.py
@@ -1,27 +1,28 @@
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
 
-chatbot= ChatBot('Victor Chaos')
+chatbot= ChatBot(
+    'Victor Chaos'
+    ,storage_adapter='chatterbot.storage.SQLStorageAdapter'
+    ,logic_adapters=['chatterbot.logic.BestMatch']
+    ,database_uri='sqlite:///database.sqlite3'
+)
 
 trainer= ListTrainer(chatbot)
 
-# trainer.train([
-#     'Well, hey fellas',
-#     'I\'ll bet you\'re looking to make yourself some money',
-#     'My name is Vic, Vic Chaos',
-#     'I\'m gonna let you in on a little secret',
-#     'Have you ever heard of NFT\'s?'
-# ])
-
 trainer.train([
-    'hi there',
-    'hello'
+    'Well, hey fellas',
+    'I\'ll bet you\'re looking to make yourself some money',
+    'My name is Vic, Vic Chaos',
+    'I\'m gonna let you in on a little secret',
+    'Have you ever heard of NFT\'s?'
 ])
-trainer.train([
-    'greetings',
-    'hello'
-])
-print("why hello there")
-response= chatbot.get_response(input())
-print(response)
 
+
+while True:
+    try:
+        user_input= input()
+        bot_response= chatbot.get_response(user_input)
+        print(bot_response)
+    except(KeyboardInterrupt,EOFError,SystemExit):
+        break
