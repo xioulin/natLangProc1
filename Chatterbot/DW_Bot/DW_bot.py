173c4a0956c58779e70c673c72f1665676171c73
Piotr Chomicki <109987107+xioulin@users.noreply.github.com>
2024-04-17 21:34:31 -0400

    updating my chatbot stuff

--- Chatterbot/DW_Bot/DW_bot.py
+++ Chatterbot/DW_Bot/DW_bot.py
@@ -1,7 +1,7 @@
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
 
-shapiroBot= ChatBot('Ben Shapiro',
+shapiroBot= ChatBot('Dave Hero',
                     storage_adapter='chatterbot.storage.SQLStorageAdapter',
                     database_uri= 'sqlite:///database.sqlite33',
                     )
