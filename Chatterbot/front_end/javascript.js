773421f4032a7cbce37a816eb23c1dcdd3b0f538
Piotr Chomicki <109987107+xioulin@users.noreply.github.com>
2024-04-18 23:09:53 -0400

    building spanish grammar page

---
+++ Chatterbot/front_end/javascript.js
@@ -0,0 +1,17 @@
+// const promptAnswer = prompt('what is your name')
+//
+// document.getElementById('prompt1').innerHTML=
+//     "Greetings,   "+ promptAnswer + "!";
+//
+
+
+
+
+// const fs= require('fs')
+//
+// fs.readFile('vocab/LaCasa.txt',(err,data)=>{
+//     if (err) throw err;
+//     console.log(data.toString())
+//     }
+//
+// )
