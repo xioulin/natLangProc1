298e030d48ef843e350766b9d2e51ecee4e8abbb
Piotr Chomicki <109987107+xioulin@users.noreply.github.com>
2024-04-22 16:57:10 -0400

    css runthrough

---
+++ front_end/javascript.js
@@ -0,0 +1,15 @@
+"use strict";
+
+let cubed = (a) =>{
+    console.log(a**3);
+}
+
+
+document.getElementById('div1').innerHTML= "Welcome to my World"
+
+function changeRed(){
+    document.body.style.backgroundColor='red'
+}
+function changeBack(){
+    document.body.style.backgroundColor='white'
+}
