23b1ff7b73ff2a0f78881952aa5894523f10e30c
Piotr Chomicki <109987107+xioulin@users.noreply.github.com>
2024-04-25 19:03:24 -0400

    models and views

--- Dead_Presidents/flaskr/flasky.py
+++ Dead_Presidents/flaskr/flasky.py
@@ -1,10 +1,27 @@
-from flask import Flask,render_template
+from flask import Flask, render_template, redirect, request, url_for
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route('/home')
 def nas():
     return "if i ruled the word"
+
+@app.route('/success/<name>')
+def success(name):
+    return 'weclome %s' % name
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        user = request.form['nm']
+        return redirect(url_for('home', name=user))
+    else:
+        user = request.args.get('nm')
+        return redirect(url_for('home', name=user))
+
+
 
 @app.route('/deadprez')
 def deadPrez():
